6a218e98269727a42276ccb53a7114ef48c64fa7
Tanish Desai <tanshikode@gmail.com>
2024-12-24 21:34:36 +0530

    Lotta changes!
    Extended map, designed BG and fixed Z-indexes, added enemy static and moving, added death animation, customized inputs, added sprite direction to knight and enemy...

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,9 +3,16 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+#	TODO: find a relation between time_scale and wait_time 	to change both with only one const val.
+	Engine.time_scale = 0.3
+	
+#	adds a mario-like drop effect on the player body
+	body.get_node("CollisionShape2D").queue_free()
+	timer.wait_time = 0.2
 	timer.start()
 	
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -12,13 +13,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis( "ui_left", "ui_right")
+	var direction := Input.get_axis( "move_left", "move_right")
 	if direction:
+		animated_sprite_2d.flip_h = true if direction==-1 else false
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
