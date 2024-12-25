6b721cf7a40ed1f03181668ecca18b592b95c279
Tanish Desai <tanshikode@gmail.com>
2024-12-25 14:18:53 +0530

    Created Coin Pickup animation
    Used animation player method

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
 	#print("+1 coin!")
-	queue_free()
+	animation_player.play("pickup_anim")
+#	TODO: try playing pickup_sound using script alone, instead of the workaround
