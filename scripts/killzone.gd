a46c0cedc9b73e30088262a85ebc2e6fecfc84b9
Tanish Desai <tanshikode@gmail.com>
2024-12-25 15:03:52 +0530

    Added run animation to player

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,15 @@
 extends Area2D
 @onready var timer: Timer = $Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
-#	TODO: find a relation between time_scale and wait_time 	to change both with only one const val.
+#	TODO: find a relation between time_scale and wait_time to change both with only one const val.
 	Engine.time_scale = 0.5
+	#body.animation_sprite_2d.play("damage") 
+#	above line throws error, fix it
 	
-#	adds a mario-like drop effect on the player body by 	freeing the collision shape
+#	adds a mario-like drop effect on the player body by freeing the collision shape
 	body.get_node("CollisionShape2D").queue_free()
 	timer.wait_time = 0.4
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,17 +12,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		#animated_sprite_2d.play("jump")
+	
+#	TODO: Add jump animation
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis( "move_left", "move_right")
 	if direction:
 		animated_sprite_2d.flip_h = true if direction==-1 else false
+		if is_on_floor():
+			animated_sprite_2d.play("run")
+		else:
+			animated_sprite_2d.play("idle")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite_2d.play("idle")
 
 	move_and_slide()
